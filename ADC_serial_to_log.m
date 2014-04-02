b1cc7adfd40930360e6f326ff189f73c3f1198c7
Eza Koch <koch.eza@gmail.com>
2014-04-02 18:01:05 -0400

    serial to file write

--- ADC_serial_to_log.m
+++ ADC_serial_to_log.m
@@ -2,8 +2,12 @@
 %
 % Reads ADC values from the M2 microcontroller.
 % saves those values to a csv.
-fname = 'data_rec.csv'
-sampling_rate = .01
+clear all;close all;clc;
+delete(instrfindall)
+
+fname = 'data_rec.csv';
+sampling_rate = .01;
+run_time = 60; %time (sec)
 
 %% VARIABLES
 
@@ -11,7 +15,7 @@ sampling_rate = .01
 M2USB = serial('/dev/tty.usbmodem411','Baudrate',9600);
 % *** Check where your device is by opening terminal and entering the command:
 % 'ls /dev/tty.usb' and tab-completing.
-M2USB.InputBufferSize=100000;
+ M2USB.InputBufferSize=100000;
 
 fopen(M2USB);       % Open up the port to the M2 microcontroller.
 flushinput(M2USB);  % Remove anything extranneous that may be in the buffer.
@@ -22,11 +26,11 @@ time = 0;       % Set the start time to 0.
 i = 1;          % Set i to 1, the indexer.
 tic;            % Start timer.
 
-
-data = zeros(5,run_time*(1/sampling_rate))
+% setup buffer for data values
+data = zeros(5,run_time*(1/sampling_rate));
 
 try
-    while toc<run_time
+    while toc < run_time
 
         %% Read in data and send confirmation packet
         m2_buffer = fgetl(M2USB);   % Load buffer
@@ -39,15 +43,14 @@ try
 
         time = toc; % Stamp the time the value was received
 
-        data(i,:) = [toc, str2double(m2_ADC1), str2double(m2_ADC2), str2double(m2_ADC3), str2double(m2_ADC4)]
-
+        data(:,i) = [toc; str2double(m2_ADC1); str2double(m2_ADC2); str2double(m2_ADC3); str2double(m2_ADC4)];
         i=i+1;  % Incrememnt indexer
 
     end
     csvwrite(fname, data)
 
-catch ME
-    ME.stack
-    %Close serial object
-    fclose(M2USB);
+  catch ME
+     ME.stack
+%      Close serial object
+     fclose(M2USB);
 end
